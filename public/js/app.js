24e6313036667a92e85b98c2991c37663d479609
unknown <morisperic54@gmail.com>
2021-02-21 18:06:40 +0100

    Removing unnecessary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -18,6 +18,7 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
+
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
