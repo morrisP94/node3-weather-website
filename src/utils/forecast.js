626daae2843bf342aff9be09a9e773f633aa427c
Moris <morisperic54@gmail.com>
2021-02-21 21:38:45 +0100

    an updated Celsius

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (longitude, latitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=c18d9559515e92bd3a42c56630e66dcf&query='+longitude+','+latitude+'&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=c18d9559515e92bd3a42c56630e66dcf&query='+longitude+','+latitude
     request({url, json:true},(error, {body}) => {
     if(error) {
         callback("Unable to connect to weather service !", undefined)
@@ -10,7 +10,7 @@ const forecast = (longitude, latitude, callback) => {
     
     } else {
         const weatherDATA = body.current
-        callback(undefined,'It is currently ' + weatherDATA.temperature + ' fahrenheit out. There is ' + weatherDATA.precip + '% to rain. It is ' + weatherDATA.weather_descriptions[0] + '.')
+        callback(undefined,'It is currently ' + weatherDATA.temperature + ' Celsius out. There is ' + weatherDATA.precip + '% to rain. It is ' + weatherDATA.weather_descriptions[0] + '.')
     }
 })
 }
